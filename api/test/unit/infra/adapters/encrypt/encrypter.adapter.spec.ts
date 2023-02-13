b75ebb21c04551f5a6132ec0c83598ef0f1e94a8
Rossano <rossanorb@gmail.com>
2023-02-13 15:55:58 -0300

    created interface to encrypter adapater

--- api/test/unit/infra/adapters/encrypt/encrypter.adapter.spec.ts
+++ api/test/unit/infra/adapters/encrypt/encrypter.adapter.spec.ts
@@ -1,11 +1,19 @@
-import { EncrypterAdapter } from "../../../../../src/infra/adapters";
+import * as bcrypt from "bcrypt";
+import EncrypterInterface from "../../../../../src/domain/adapters/encrypter.interface";
+
+export class EncrypterAdapteSpy implements EncrypterInterface {
+    encrypt(input: string): string {
+        return bcrypt.hashSync(input, 7);
+    }
+    compare(input: string, inputEncrypted: string): boolean {
+        return bcrypt.compareSync(input, inputEncrypted);
+    }
+}
 
 describe("Test Encripter", () => {
     it("Should encripter a passoword", () => {
-        const encryper = new EncrypterAdapter();
+        const encryper = new EncrypterAdapteSpy();
         const encripted = encryper.encrypt("@taKemi389");
-        expect(encripted).toHaveLength(60);
-        expect(encripted !== "@taKemi389").toBeTruthy();
         expect(encryper.compare("@taKemi389", encripted)).toBeTruthy();
     });
 });
